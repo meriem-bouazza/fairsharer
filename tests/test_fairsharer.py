4271aaff5d1d929e194d5de6b97e7430899c38c9
Meriem Bouazza <meriem.bouazzastudy.hs-duesseldorf.de>
2026-01-11 22:44:35 +0100

    Added unit test for fair_sharer (task c)

--- tests/test_fairsharer.py
+++ tests/test_fairsharer.py
@@ -1,4 +1,10 @@
-"""
-Tests for the fairsharer
-"""
+from fairsharer.fairsharer import fair_sharer
 
+def test_fair_sharer():
+
+    # Test examples from the Taskcheat in floats
+    result_1 = fair_sharer([0, 1000, 800, 0], 1)
+    assert result_1 == [100.0, 800.0, 900.0, 0.0]
+
+    result_2 = fair_sharer([0, 1000, 800, 0], 2)
+    assert result_2 == [100.0, 890.0, 720.0, 90.0]
